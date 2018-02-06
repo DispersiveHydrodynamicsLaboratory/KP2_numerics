34e860b1a5ac23842ac65559224fb81fb4eeea47
Michelle M <Michelle.Maiden@colorado.edu>
2018-02-06 16:25:05 -0700

    Working version, does NOT rescale to [-pi,pi]

--- KP_numerics-master/solver/driver_KP_solver_standard.m
+++ KP_numerics-master/solver/driver_KP_solver_standard.m
@@ -2,6 +2,8 @@
 %% Domain, ICs, etc go here
 save_on  = 1;  % Set to nonzero if you want to run the solver, set
                % to 0 if you want to plot
+rmdir_on = 1;  % Set to nonzero if you want to delete and remake the chosen directory
+               % Useful for debugging
 periodic = 1;  % setto nonzero to run periodic solver (no BCs need)
                % set to 0 to run solver with time-dependent BCs                
 plot_on  = 1;  % Set to 1 if you want to plot just before and just
@@ -12,47 +14,17 @@ check_IC = 0;  % Set to nonzero to plot the ICs and BCs without running the solv
 tmax   = 5;      % Solver will run from t=0 to t = tmax
 numout = 10*tmax+1; % numout times will be saved (including ICs)
 Lx     = 50;     % Solver will run on x \in [-Lx,Lx]
-Ly     = 30;     % Solver will run on y \in [-Ly,Ly]
-Nx     = 2^8;    % Number of Fourier modes in x-direction
+Ly     = Lx/2;     % Solver will run on y \in [-Ly,Ly]
+Nx     = 2^9;    % Number of Fourier modes in x-direction
 Ny     = 2^8;    % Number of Fourier modes in y-direction
 
 t      = linspace(0,tmax,numout);
 
 %% Initial Condition and large-y approximation in time
-    ic_type = 'RP_soli';
-    %% One-soliton
-    	sa = 1; q = 1;
-    a     = @(x,y,t) sa^2 .* ones(size(x));
-    ax    = @(x,y,t) zeros(size(x));
-    ay    = @(x,y,t) zeros(size(x));
-    q     = @(x,y,t)  q   .* ones(size(x));
-    qx    = @(x,y,t) zeros(size(x));
-    qy    = @(x,y,t) zeros(size(x));
-    uasoli = @(theta,x,y,t,a) a(x,y,t).*(sech(sqrt(a(x,y,t)/12).*theta)).^2;
-  dxuasoli = @(theta,dxtheta,x,y,t,a,ax) sech(sqrt(a(x,y,t)/12).*theta).^2.*...
-               (ax(x,y,t) + sqrt(a(x,y,t)/12).*tanh(sqrt(a(x,y,t)/12).*theta).*...
-                    dxtheta);
-  dyuasoli = @(theta,dytheta,x,y,t,a,q) sech(sqrt(a(x,y,t)/12).*theta).^2.*...
-               (ay(x,y,t) + sqrt(a(x,y,t)/12).*tanh(sqrt(a(x,y,t)/12).*theta).*...
-                    dytheta);
-
-    theta = @(x,y,t,a,q) (x + q(x,y,t).*y - (a(x,y,t)/3+q(x,y,t).^2) * t);
-  dxtheta = @(x,y,t,a,ax,q,qx) (-(x+(y-t.*q(x,y,t)).*q(x,y,t))*ax(x,y,t)+...
-                    a(x,y,t).*(-2+t*ax(x,y,t)-2*(y-2*t*q(x,y,t)).*qx(x,y,t)));
-  dytheta = @(x,y,t,a,ay,q,qy) (-(x+(y-t.*q(x,y,t)).*q(x,y,t)).*ay(x,y,t)+...
-                    a(x,y,t).*(t.*ay(x,y,t)-2*y.*qy(x,y,t)+...
-                    q(x,y,t)*(-2+4*t.*qy(x,y,t))));
-  
-                       
-    uasy  = @(x,y,t)  uasoli(theta(x,y,t,a,q),x,y,t,a);
-  dxuasy  = @(x,y,t) dxuasoli(theta(x,y,t,a,q),...
-                            dxtheta(x,y,t,a,ax,q,qx),...
-                             x,y,t,a,ax);
-  dyuasy  = @(x,y,t) dyuasoli(theta(x,y,t,a,q),...
-                            dxtheta(x,y,t,a,ax,q,qx),...
-                              x,y,t,a,q);
-
-    u0    = @(x,y)    uasy(x,y,0);
+    ic_type = 'KP2_soli';
+    %% One-soliton, corrected for nonzero integral in x
+    	sa = 0.5; q = 0.75; x0 = 0;
+        [ soli ] = one_soli(sa,q,x0,Lx);
 
     
    %% Two-Soliton amplitude, angle parameters
@@ -93,20 +65,27 @@ if ~exist(data_dir,'dir')
     mkdir(data_dir);
 else
     disp(['Warning, directory ',data_dir]);
-    disp('already exists, possibly overwriting data');
+    if rmdir_on == 1
+        disp('already exists, rewriting entire folder');
+        rmdir(data_dir,'s')
+        mkdir(data_dir);
+    else
+        disp('already exists, possibly overwriting data');
+    end
 end
 
 savefile = sprintf('%sparameters.mat',data_dir);
 
 %% If chosen, run the solver using the parameters and conditions above
 if save_on
-    % Load initial data
-      xplot  = (2*Lx/Nx)*[-Nx/2:Nx/2];
-      yplot  = (2*Ly/Ny)*[-Ny/2:Ny/2];
-      [XPLOT,YPLOT] = meshgrid(xplot,yplot);
-      tplot  = linspace(0,tmax,floor(tmax*10));
-      u_init = u0(XPLOT,YPLOT);
     if plot_on
+        % Load initial data
+          xplot  = (2*Lx/Nx)*[-Nx/2:Nx/2-1];
+          yplot  = (2*Ly/Ny)*[-Ny/2:Ny/2-1];
+          [XPLOT,YPLOT] = meshgrid(xplot,yplot);
+          tplot  = linspace(0,tmax,floor(tmax*10));
+          u_init = soli.u0(XPLOT,YPLOT);
+
         % Plot initial conditions and boundary conditions
         fontsize = 12;
         figure(1); clf;
@@ -114,13 +93,13 @@ if save_on
             contourf(XPLOT,YPLOT,u_init,100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
             title('Initial Conditions');
         subplot(2,2,2)
-            contourf(XPLOT,YPLOT,uasy(XPLOT,YPLOT,0),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
+            contourf(XPLOT,YPLOT,soli.ua(XPLOT,YPLOT,0),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
             title('Asymptotic u');
         subplot(2,2,3)
-            contourf(XPLOT,YPLOT,dxuasy(XPLOT,YPLOT,0),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
+            contourf(XPLOT,YPLOT,soli.uax(XPLOT,YPLOT,0),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
             title('Asymptotic u, x-deriv');
         subplot(2,2,4)
-            contourf(XPLOT,YPLOT,dyuasy(XPLOT,YPLOT,0),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
+            contourf(XPLOT,YPLOT,soli.uay(XPLOT,YPLOT,0),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
             title('Asymptotic u, y-deriv');
         set(gca,'fontsize',fontsize,'fontname','times');
         pause(0.25);
@@ -135,12 +114,12 @@ if save_on
     % Save parameters
         save(savefile,'t','Nx','Lx',...
                           'Ny','Ly',...
-                          'u0','uasy','dxuasy','dyuasy',...
+                          'soli',...
                           'periodic');
     % Run timestepper
         KP_solver_periodic( t, Lx, Nx,...
                                Ly, Ny,...
-                               u0, uasy, dxuasy,dyuasy,...
+                               soli,...
                                data_dir );     
 else
     load(savefile);
---
+++ KP_numerics-master/solver/one_soli.m
@@ -0,0 +1,79 @@
+function [ soli ] = one_soli( sa, qa, x0, Lx )
+%Creates a structure that's a soliton IC
+% Inputs
+% sa: square root of the amplitude
+% qa: slope of the angle of propagation
+
+verbose = 1; % nonzero means plotting, text output, etc
+
+%% Define Soliton parameters and derivatives in x and y
+    soli.a     = @(x,y,t) sa^2 .* ones(size(x));
+    soli.ax    = @(x,y,t) zeros(size(x));
+    soli.ay    = @(x,y,t) zeros(size(x));
+    soli.q     = @(x,y,t)  qa   .* ones(size(x));
+    soli.qx    = @(x,y,t) zeros(size(x));
+    soli.qy    = @(x,y,t) zeros(size(x));
+    soli.x0    = x0;
+    
+%% Galilean boost to correct for nonzero mean
+    u  = @(theta,x,y,t,a) a(x,y,t).*(sech(sqrt(a(x,y,t)/12).*theta)).^2;
+    th = @(x,y,t,a,q) (x + q(x,y,t).*y - (a(x,y,t)/3+q(x,y,t).^2) * t);
+    I = integral(@(x) u(th(x,0,0,soli.a,soli.q),x,0,0,soli.a), -Lx, Lx);
+    soli.G = -I/(2*Lx);
+    if verbose
+        disp(['Integral is: ',num2str(I)]);
+    end
+%% Define Soliton function, derivatives
+    soli.u  = @(theta,x,y,t,a,g) g + a(x,y,t).*(sech(sqrt(a(x,y,t)/12).*theta)).^2;
+    soli.dx = @(theta,dxtheta,x,y,t,a,ax) sech(sqrt(a(x,y,t)/12).*theta).^2.*...
+               (ax(x,y,t) + sqrt(a(x,y,t)/12).*tanh(sqrt(a(x,y,t)/12).*theta).*...
+                    dxtheta);
+    soli.dy = @(theta,dytheta,x,y,t,a,q,ay) sech(sqrt(a(x,y,t)/12).*theta).^2.*...
+               (ay(x,y,t) + sqrt(a(x,y,t)/12).*tanh(sqrt(a(x,y,t)/12).*theta).*...
+                    dytheta);
+% Theta and derivatives
+    soli.th = @(x,y,t,a,q,g) (x + q(x,y,t).*y - (a(x,y,t)/3+q(x,y,t).^2+6*g) * t);
+    soli.thx = @(x,y,t,a,ax,q,qx) (-(x+(y-t.*q(x,y,t)).*q(x,y,t)).*ax(x,y,t)+...
+                        a(x,y,t).*(-2+t*ax(x,y,t)-2*(y-2*t*q(x,y,t)).*qx(x,y,t)));
+    soli.thy = @(x,y,t,a,ay,q,qy) (-(x+(y-t.*q(x,y,t)).*q(x,y,t)).*ay(x,y,t)+...
+                        a(x,y,t).*(t.*ay(x,y,t)-2*y.*qy(x,y,t)+...
+                        q(x,y,t).*(-2+4*t.*qy(x,y,t))));
+  
+% Asymptotic soliton approximation                       
+    soli.ua  = @(x,y,t)  soli.u(soli.th(x-soli.x0,y,t,soli.a,soli.q,soli.G),x-soli.x0,y,t,soli.a,soli.G);
+    soli.uax  = @(x,y,t) soli.dx(soli.th(x-soli.x0,y,t,soli.a,soli.q,soli.G),...
+                            soli.thx(x-soli.x0,y,t,soli.a,soli.ax,soli.q,soli.qx),...
+                             x-soli.x0,y,t,soli.a,soli.ax);
+    soli.uay  = @(x,y,t) soli.dy(soli.th(x-soli.x0,y,t,soli.a,soli.q,soli.G),...
+                                 soli.thy(x-soli.x0,y,t,soli.a,soli.ay,soli.q,soli.qy),...
+                                  x-soli.x0,y,t,soli.a,soli.q,soli.ay);
+% Initial condition
+    soli.u0    = @(x,y)    soli.ua(x,y,0);
+
+        % Figure for debugging
+        if verbose
+            xplot = -Lx:0.5:Lx;
+            yplot = -Lx/2:0.5:Lx/2;
+            [XPLOT,YPLOT] = meshgrid(xplot,yplot);
+            figure(1); clf;
+            subplot(2,2,1)
+                contourf(XPLOT,YPLOT,soli.u0(XPLOT,YPLOT),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
+                title('Initial Conditions');
+            for ti = linspace(0,5,2)
+                subplot(2,2,2)
+                    contourf(XPLOT,YPLOT,soli.ua(XPLOT,YPLOT,ti),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
+                    title(['Asymptotic u, t=',num2str(ti)]);
+                subplot(2,2,3)
+                    contourf(XPLOT,YPLOT,soli.uax(XPLOT,YPLOT,ti),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
+                    title('Asymptotic u, x-deriv');
+                subplot(2,2,4)
+                    contourf(XPLOT,YPLOT,soli.uay(XPLOT,YPLOT,ti),100,'edgecolor','none'); xlabel('x'); ylabel('y'); 
+                    title('Asymptotic u, y-deriv');
+    %             set(gca,'fontsize',fontsize,'fontname','times');
+                drawnow;
+                pause(0.25);
+            end
+        end
+
+end
+
